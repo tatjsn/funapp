9a78845f7dfb90bf6e07891822542e113a497415
Taz Jackson <tatjsn@users.noreply.github.com>
2017-02-18 01:12:51 +0900

    Better cache handling

--- src/sw.js
+++ src/sw.js
@@ -1,18 +1,31 @@
 /* global self, caches, fetch */
+const version = '%%hash%%';
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('cache-%%hash%%')
+    caches.open(`cache-${version}`)
       .then(cache => cache.addAll([
         '/',
         '/main.js',
         '//cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css',
       ])
-  ));
+      ));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => Promise.all(
+        cacheNames.map(cacheName =>
+          cacheName !== `cache-${version}` && caches.delete(cacheName)
+        )
+      ))
+  );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
+    caches.match(event.request, { ignoreSearch: true })
       .then(response => response || fetch(event.request))
   );
 });
